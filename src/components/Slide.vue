ee3ec8a08baf9ffb35218c16a85ed836d3ae3891
CesarWatrin <cesar.watrin@hotmail.com>
2022-12-10 15:30:31 +0100

    feat: cut into components

---
+++ src/components/Slide.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+const props = defineProps<{
+  image: string;
+}>();
+</script>
+
+<template>
+  <div
+    :style="`background-image: url('/src/assets/images/${props.image}');`"
+    class="min-w-[500px] h-[430px] bg-center bg-cover bg-no-repeat rounded-lg mx-4"
+  ></div>
+</template>
--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -1,11 +1,43 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { ref } from "vue";
+import Slide from "@/components/Slide.vue";
+
+const images = ref<Array<string>>([
+  "image_1.jpg",
+  "image_2.jpg",
+  "image_3.jpg",
+  "image_4.jpg",
+  "image_5.jpg",
+  "image_6.jpg",
+  "image_7.jpg",
+]);
+
+const next = () => {
+  return true;
+};
+
+const back = () => {
+  return true;
+};
+</script>
 
 <template>
-  <div class="p-4">
-    <div>
-      <!--component slide-->
-      <div style="background: url('')">
-
+  <div class="py-8 px-4 bg-white overflow-hidden">
+    <div class="flex">
+      <Slide v-for="(image, idx) in images" :key="idx" :image="image" />
+    </div>
+    <div class="mt-8 mx-4 flex">
+      <div
+        class="py-4 px-8 mr-4 bg-amber-500 rounded-lg cursor-pointer"
+        @click="back"
+      >
+        Back
+      </div>
+      <div
+        class="py-4 px-8 mr-4 bg-amber-500 rounded-lg cursor-pointer"
+        @click="next"
+      >
+        Next
       </div>
     </div>
   </div>
